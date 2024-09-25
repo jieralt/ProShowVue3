44c531035a48423788525331064e3257d101a48f
jieralt <jieralt@163.com>
2024-09-25 17:20:30 +0800

    1

--- web-proshow/vue.config.js
+++ web-proshow/vue.config.js
@@ -6,9 +6,27 @@ module.exports = defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://127.0.0.1:5000',  // 使用 IP 地址而不是 localhost
+        changeOrigin: true
+      },
+      '/static': {
+        target: 'http://127.0.0.1:5000',  // 同样更新这里
         changeOrigin: true
       }
     }
   }
 })
+
+// module.exports = {
+//   transpileDependencies: true,
+//   devServer: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//         ws: true,
+//       }
+//     }
+//   }
+// }
